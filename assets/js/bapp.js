cb8c415f906c3bb00475424416f90b0c2a2a8be0
Rodollfo09899 <58276692+Rodollfo09899@users.noreply.github.com>
2026-03-07 18:37:11 -0800

    Update business details and add new menu items

--- assets/js/bapp.js
+++ assets/js/bapp.js
@@ -2,25 +2,128 @@
   "business": {
     "phoneE164": "+14153496404",
     "whatsAppPhoneE164": "14153496404",
-    "instagramUrl": "https://www.instagram.com/catering_jr04",
+    "instagramUrl": "https://www.instagram.com/enguayabado_01",
     "googleReviewUrl": "PASTE_TU_LINK_DE_REVIEW_GOOGLE",
     "yelpUrl": "PASTE_TU_LINK_YELP"
   },
   "categories": [
     "Especialidades",
     "Tacos",
+    "Quesabirria",
+    "Salchipapa",
+    "Choripán",
     "Patacones / Arepas",
     "Tostadas",
     "Ramen",
-    "Bebidas Frías"
+    "Bebidas Frías",
+    "Add Ons / Extras"
   ],
   "items": [
-    { "id": "enguayabado_signature", "name": "Enguayabado (Signature)", "category": "Especialidades", "price": 18.0, "description": "Tu plato estrella." },
-    { "id": "tacos_mini", "name": "Mini Tacos (Orden)", "category": "Tacos", "price": 14.0, "description": "Elige proteína." },
-    { "id": "patacon", "name": "Patacón", "category": "Patacones / Arepas", "price": 13.0, "description": "Crocante con toppings y salsa." },
-    { "id": "arepa", "name": "Arepa", "category": "Patacones / Arepas", "price": 12.0, "description": "Rellena (edita)." },
-    { "id": "tostada", "name": "Tostada", "category": "Tostadas", "price": 10.0, "description": "Con proteína y salsas." },
-    { "id": "ramen", "name": "Ramen (Fusión)", "category": "Ramen", "price": 16.0, "description": "Ramen con tu toque." },
-    { "id": "mangonada", "name": "Mangonada", "category": "Bebidas Frías", "price": 9.0, "description": "Mango frozen + chamoy + tajín." }
+    {
+      "id": "enguayabado",
+      "name": "Enguayabado (Signature)",
+      "category": "Especialidades",
+      "description": "Tu plato estrella.",
+      "price": 18
+    },
+    {
+      "id": "mini-tacos",
+      "name": "Mini Tacos (Orden)",
+      "category": "Tacos",
+      "description": "Elige proteína.",
+      "price": 14
+    },
+    {
+      "id": "patacon",
+      "name": "Patacón",
+      "category": "Patacones / Arepas",
+      "description": "Crocante con toppings y salsa.",
+      "price": 13
+    },
+    {
+      "id": "arepa",
+      "name": "Arepa",
+      "category": "Patacones / Arepas",
+      "description": "Rellena.",
+      "price": 12
+    },
+    {
+      "id": "tostada",
+      "name": "Tostada",
+      "category": "Tostadas",
+      "description": "Con proteína y salsas.",
+      "price": 10
+    },
+    {
+      "id": "ramen",
+      "name": "Ramen (Fusión)",
+      "category": "Ramen",
+      "description": "Ramen con tu toque.",
+      "price": 16
+    },
+    {
+      "id": "mangonada",
+      "name": "Mangonada",
+      "category": "Bebidas Frías",
+      "description": "Mango frozen + chamoy + tajín.",
+      "price": 9
+    },
+
+    {
+      "id": "extra-pollo",
+      "name": "Extra Pollo",
+      "category": "Add Ons / Extras",
+      "description": "Agrega pollo.",
+      "price": 3
+    },
+    {
+      "id": "extra-birria",
+      "name": "Extra Birria",
+      "category": "Add Ons / Extras",
+      "description": "Agrega birria.",
+      "price": 5
+    },
+    {
+      "id": "extra-asada",
+      "name": "Extra Asada",
+      "category": "Add Ons / Extras",
+      "description": "Agrega carne asada.",
+      "price": 5
+    },
+    {
+      "id": "extra-camarones",
+      "name": "Extra Camarones",
+      "category": "Add Ons / Extras",
+      "description": "Agrega camarones.",
+      "price": 5
+    },
+    {
+      "id": "extra-egg",
+      "name": "Poached Egg / Fried Egg",
+      "category": "Add Ons / Extras",
+      "description": "Huevo extra.",
+      "price": 3
+    },
+    {
+      "id": "extra-bacon",
+      "name": "Extra Bacon",
+      "category": "Add Ons / Extras",
+      "description": "Tocino extra.",
+      "price": 3
+    },
+    {
+      "id": "extra-aguacate",
+      "name": "Extra Aguacate",
+      "category": "Add Ons / Extras",
+      "description": "Aguacate extra.",
+      "price": 3
+    },
+    {
+      "id": "extra-maduritos",
+      "name": "Maduritos",
+      "category": "Add Ons / Extras",
+      "description": "Porción de maduritos.",
+      "price": 5
+    }
   ]
 }
